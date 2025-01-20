6fbe03c3a0d8e1f415f5525af95ab9843556fa63
bruwwu <bruwutuwu@gmail.com>
2025-01-20 17:50:13 -0600

    aa

---
+++ Include/Prerequisites.h
@@ -0,0 +1,58 @@
+
+#pragma once
+// Librerias STD
+#include <string>
+#include <sstream>
+#include <vector>
+#include <windows.h>
+#include <xnamath.h>
+//#include <memory>
+#include <thread>
+
+// Librerias DirectX
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dcompiler.h>
+#include "Resource.h"
+#include "resource.h"
+
+// MACROS
+#define SAFE_RELEASE(x) if(x != nullptr) x->Release(); x = nullptr;
+
+#define MESSAGE( classObj, method, state )   \
+{                                            \
+   std::wostringstream os_;                  \
+   os_ << classObj << "::" << method << " : " << "[CREATION OF RESOURCE " << ": " << state << "] \n"; \
+   OutputDebugStringW( os_.str().c_str() );  \
+}
+
+#define ERROR( classObj, method, errorMSG )  \
+{                                            \
+   std::wostringstream os_;                  \
+   os_ << "ERROR : " << classObj << "::" << method << " : " << "  Error in data from params [" << errorMSG << "] \n"; \
+   OutputDebugStringW( os_.str().c_str() );  \
+   exit(1);                                  \
+}
+
+// Structures
+struct
+	SimpleVertex {
+	XMFLOAT3 Pos;
+	XMFLOAT2 Tex;
+};
+
+struct
+	CBNeverChanges {
+	XMMATRIX mView;
+};
+
+struct
+	CBChangeOnResize {
+	XMMATRIX mProjection;
+};
+
+struct
+	CBChangesEveryFrame {
+	XMMATRIX mWorld;
+	XMFLOAT4 vMeshColor;
+};
